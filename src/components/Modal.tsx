3fee13527d31e81efcaeeae43e602e44a969d519
AntoninaSolodskaya <36710927+AntoninaSolodskaya@users.noreply.github.com>
2021-02-23 21:50:08 +0200

    add pagination

---
+++ src/components/Modal.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+export default function Modal() {
+  const [showModal, setShowModal] = React.useState(true);
+  return (
+    <>
+      {showModal ? (
+        <div className="relative">
+          <div
+            className={`p-2 ${
+              showModal
+                ? "bg-opacity-25 bg-purple-600 text-red-500 p-20 sm:px-6 pointer-events-none"
+                : "bg-red-500 text-white p-20 sm:px-6"
+            }`}
+            onClick={() => setShowModal(true)}
+            style={{ opacity: 0.5 }}
+          >
+            <p className="text-6xl">fdhgghfh</p>
+          </div>
+          <div
+            className="absolute justify-center items-center inset-1 z-50 outline-none focus:outline-none"
+            style={{
+              top: "0",
+              bottom: "0",
+              left: "50%"
+            }}
+            onClick={() => setShowModal(false)}
+          >
+            <div className="absolute w-auto p-6 my-2 mx-auto max-w-sm">
+              <div className="border-0 rounded-lg shadow-lg flex flex-col w-full bg-white outline-none focus:outline-none">
+                <div className="flex items-start p-3 justify-between p-2 border-b border-solid border-gray-300 rounded-t">
+                  <h3 className="text-3sm font-semibold">Modal Title</h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative flex-auto">
+                  <p className="my-2 text-gray-600 p- text-sm leading-relaxed">
+                    I always felt like I could do anything
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div
+          class="bg-red-500 text-white p-20 sm:px-6"
+          onClick={() => setShowModal(true)}
+        >
+          fdhg
+        </div>
+      )}
+    </>
+  );
+}
---
+++ src/components/Todo.tsx
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from "react";
+
+const data = [
+  { id: 1, title: "test" },
+  { id: 2, title: "test2" },
+  { id: 3, title: "test3" },
+  { id: 4, title: "test4" },
+  { id: 5, title: "test5" },
+  { id: 6, title: "test6" },
+  { id: 7, title: "test6" },
+  { id: 8, title: "test6" },
+  { id: 9, title: "test6" },
+  { id: 10, title: "test6" },
+  { id: 11, title: "test11" },
+  { id: 12, title: "test12" },
+  { id: 13, title: "test13" },
+  { id: 14, title: "test14" },
+  { id: 15, title: "test15" },
+  { id: 16, title: "test16" },
+  { id: 17, title: "test16" },
+  { id: 18, title: "test16" },
+  { id: 19, title: "test126" },
+  { id: 20, title: "test20" }
+];
+
+const Todo = () => {
+  const [posts, setPosts] = useState(data);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(3);
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  console.log(posts);
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(posts.length / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+  const isDisable = posts.length > pageNumbers.length;
+  return (
+    <>
+      {currentPosts.map(item => (
+        <div key={item.id} className="post">
+          <h3>
+            {item.title} - {item.id}
+          </h3>
+        </div>
+      ))}
+
+      <nav className="flex">
+        {pageNumbers.map((number, i) => (
+          <div key={i}>
+            <button
+              type="button"
+              disabled={true}
+              className={`py-2 px-4 mx-2 rounded-full text-gray-400 ${
+                isDisable
+                  ? "bg-purple-600 opacity-25 pointer-events-none"
+                  : "bg-purple-600"
+              }`}
+              onClick={() => paginate(number)}
+            >
+              {number}
+            </button>
+          </div>
+        ))}
+      </nav>
+    </>
+  );
+};
+
+export default Todo;
